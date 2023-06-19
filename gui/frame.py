4094fd2d640b63f9596406af88b91d902a4f1f8e
Rohit C <124383636+R021T@users.noreply.github.com>
2023-06-19 10:45:40 +0530

    Update frame.py

--- gui/frame.py
+++ gui/frame.py
@@ -1,5 +1,4 @@
 from tkinter import *
-#from tkinter.ttk import *
 from PIL import Image,ImageTk
 
 top = Tk()
@@ -15,7 +14,7 @@ p1=Image.open("C:/Desktop Wallpapers/2517915.jpg")
 p1=p1.resize((130,130),Image.ANTIALIAS)
 photoimg=ImageTk.PhotoImage(p1)
 lbl_img=Label(image=photoimg)
-lbl_img.place(x=40,y=90,width=70,height=70)
+lbl_img.place(x=30,y=90,width=110,height=70)
 box1=Text(f1,height=2,width=25).place(x=150,y=110)
 b2=Button(f1,height=2,width=5,text="+").place(x=370,y=110)
 #image2
@@ -23,13 +22,13 @@ p2=Image.open("C:/Desktop Wallpapers/2517915.jpg")
 p2=p2.resize((130,130),Image.ANTIALIAS)
 photoimg2=ImageTk.PhotoImage(p2)
 lbl_img2=Label(image=photoimg2)
-lbl_img2.place(x=40,y=170,width=70,height=70)
+lbl_img2.place(x=30,y=170,width=110,height=70)
 #image3
 p3=Image.open("C:/Desktop Wallpapers/2517915.jpg")
 p3=p3.resize((130,130),Image.ANTIALIAS)
 photoimg3=ImageTk.PhotoImage(p3)
 lbl_img3=Label(image=photoimg3)
-lbl_img3.place(x=40,y=250,width=70,height=70)
+lbl_img3.place(x=30,y=250,width=110,height=70)
 
 box1=Text(f1,height=2,width=25).place(x=150,y=110)
 b2=Button(f1,height=2,width=5,text="+").place(x=370,y=110)
